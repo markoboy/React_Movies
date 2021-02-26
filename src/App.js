1f9105b06ab473c867a5d2c30174efaa9f9b366a
Athanasios Markou <Athanasios_Markou@epam.com>
2021-02-26 15:34:24 +0000

    Feat: Scalfold scss folder with files that will be used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.scss';
 
 export default function App() {
   return (
     <div className="app">
       <h1>Hello React!</h1>
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+
+      <button type="button">Test</button>
     </div>
   );
 }
