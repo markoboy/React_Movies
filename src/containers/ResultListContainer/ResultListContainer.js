e8d81629df6b1d4fbde34321c56f1b373f84af70
Athanasios Markou <Athanasios_Markou@epam.com>
2021-03-01 10:25:59 +0000

    Update: Remove random error from ResultListContainer

--- src/containers/ResultListContainer/ResultListContainer.js
+++ src/containers/ResultListContainer/ResultListContainer.js
@@ -2,7 +2,6 @@ import withSection from '@components/HigherOrder/WithSection';
 import ResultCountComponent from '@components/ResultCountComponent/ResultCountComponent';
 import ResultListComponent from '@components/ResultListComponent/ResultListComponent';
 import ResultListItemComponent from '@components/ResultListItemComponent/ResultListItemComponent';
-import LoggerService from '@services/LoggerService';
 import MovieService from '@services/MovieService';
 import React from 'react';
 
@@ -10,17 +9,10 @@ const ResultListComponentWithSection = withSection(ResultListComponent, {
   classes: 'section--padding-bottom-only',
 });
 
-const logger = new LoggerService('ResultListContainer');
-
 export default function ResultListContainer() {
   const movies = MovieService.getMovies();
 
   const actions = ['Edit', 'Delete'];
-
-  logger.info('Math random is called to trigger Error Boundary with a 50% chance!');
-  if (Math.random() <= 0.5) {
-    throw new Error('An error occurred!');
-  }
 
   return (
     <>
