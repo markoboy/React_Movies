118c5db27addae3b6904cced685d47ea0cd94521
Athanasios Markou <Athanasios_Markou@epam.com>
2021-03-03 09:35:25 +0000

    feat: Add header nav with background

--- src/components/HeaderNavComponent/HeaderNavComponent.js
+++ src/components/HeaderNavComponent/HeaderNavComponent.js
@@ -8,25 +8,31 @@ import PropTypes from 'prop-types';
  * @param {Object} props React props
  * @param {import('react').Component} props.brandLogo The header's logo
  * @param {import('react').Component} props.actionButton The header's action button
+ * @param {Boolean} props.hasBackground If the header should have a background
  */
-function HeaderNavComponent({ headerLogo, actionButton }) {
+function HeaderNavComponent({ headerLogo, actionButton, hasBackground }) {
   return (
-    <header className="header-nav-container">
+    <header
+      className={`header-nav-container ${
+        hasBackground ? 'header-nav-container--with-bg' : ''
+      }`}
+    >
       <div className="header-nav">
-        <div className="header-nav__logo-container">
-          {headerLogo}
-        </div>
-        <div className="header-nav__btn-container">
-          {actionButton}
-        </div>
+        <div className="header-nav__logo-container">{headerLogo}</div>
+        <div className="header-nav__btn-container">{actionButton}</div>
       </div>
     </header>
   );
 }
 
+HeaderNavComponent.defaultProps = {
+  hasBackground: false,
+};
+
 HeaderNavComponent.propTypes = {
   headerLogo: PropTypes.element.isRequired,
   actionButton: PropTypes.element.isRequired,
+  hasBackground: PropTypes.bool,
 };
 
 export default HeaderNavComponent;
