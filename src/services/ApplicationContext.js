43741cc5f814f99dca793d17df5b9849ea5411d3
Athanasios Markou <thanasismarko@gmail.com>
2021-03-05 18:29:26 +0000

    clean: Remove unused files

--- src/services/ApplicationContext.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { createContext } from 'react';
-
-export const defaultApplicationContext = {
-  selectedMovieId: null,
-  setSelectedMovieId() {},
-};
-
-export const ApplicationContext = createContext(defaultApplicationContext);
